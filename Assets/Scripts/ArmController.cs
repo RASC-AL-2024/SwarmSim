58cf2cf0875b317e7dd9117b49f9dfe0b4c7c996
markhalka <mahalka@uwaterloo.ca>
2024-02-20 18:08:32 -0500

    added arm model, and inverse kinematics controller

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    // add two local way points between which the arm will move, basically picking up material and placing it when needed (when a rover is near)
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ArmJoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmJoint : MonoBehaviour {
+    public Vector3 StartOffset;
+    private Transform _transform;
+    public char _rotationAxis;
+    public Vector3 RotationAxis;
+
+    private void Awake() {
+        _transform = this.transform;
+        StartOffset = _transform.localPosition;
+        switch (_rotationAxis)
+        {
+            case 'x':
+                RotationAxis = Vector3.right;
+                break;
+            case 'y':
+                RotationAxis = Vector3.up;
+                break;
+            case 'z':
+                RotationAxis = Vector3.forward;
+                break;
+        }
+    }
+
+}
