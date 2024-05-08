de484e0676a8f3c6bba5e211d41896221d7bd18f
Mark Halka <mahalka@uwaterloo.ca>
2024-05-08 13:28:29 -0700

    added plotting for total dirt

--- Assets/Scripts/CentralResources.cs
+++ Assets/Scripts/CentralResources.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.IO;
+using System;
 
 public class CentralResources : MonoBehaviour
 {
     // I hate C#
     public Battery Battery { get; set; } = new Battery(Constants.centralBatteryCapacity, Constants.centralBatteryCapacity / 2);
-
+    public static Storage TotalDirt { get; set; } = new Storage(double.MaxValue, 0); // may the lord forgive me
     public Storage Dirt { get; set; } = new Storage(Constants.centralDirtCapacity, 0);
     public Storage Powder { get; set; } = new Storage(Constants.centralDirtCapacity, 0);
     public Storage PrintMaterial { get; set; } = new Storage(Constants.centralDirtCapacity, 0);
     public Storage SpareModules { get; set; } = new Storage(Constants.spareModuleCapacity, 0);
-
     void Start()
     {
         StartCoroutine(Log());
@@ -20,12 +20,12 @@ public class CentralResources : MonoBehaviour
     IEnumerator Log()
     {
         var writer = new StreamWriter("resourceData.csv");
-        writer.WriteLine("time,battery,spareModules");
+        writer.WriteLine("time,battery,spareModules,totalDirt");
         var nextTime = Time.time;
         while (true)
         {
             nextTime += 60;
-            writer.WriteLine("{0},{1},{2}", Time.time, Battery.current, SpareModules.current);
+            writer.WriteLine("{0},{1},{2}, {3}", Time.time, Battery.current, SpareModules.current, TotalDirt.current);
             writer.Flush();
             yield return new WaitForSeconds(nextTime - Time.time);
         }
--- Misc/resourceDataPlotter.py
+++ Misc/resourceDataPlotter.py
@@ -17,20 +17,6 @@ ax1.set_ylabel('Battery')
 ax1.grid(True)
 ax1.legend()
 
-def calculate_positive_deltas(values):
-    running_totals = [0]
-    current_total = 0
-
-    for i in range(1, len(values)):
-        delta = values[i] - values[i - 1]
-        if delta > 0:
-            current_total += delta
-        running_totals.append(current_total)
-
-    return running_totals
-
-# total_spares = calculate_positive_deltas(df['spareModules'])
-
 ax2.plot(df['time'], df['totalDirt'], label='Total Dirt', color='green')
 ax2.set_title('Total Dirt Over Time')
 ax2.set_xlabel('Time')
